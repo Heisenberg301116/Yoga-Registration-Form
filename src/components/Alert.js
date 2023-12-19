01f319b33fd0bdfb24fa92b841cf0e679405d965
Sushant <sushant889427@gmail.com>
2023-12-19 18:28:13 +0530

    Modified Form container

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,13 +1,13 @@
 function Alert(props) {
     if (props.obj == null) {      // when alert is set to null, don't display anything
         return (
-            <div style={{ height: '60px', marginTop: '1px', marginBottom: '25px' }}>
+            <div style={{ height: '60px', marginTop: '1px', marginBottom: '5px' }}>
             </div>
         )
     }
     else {
         return (
-            <div style={{ height: '60px', marginTop: '1px', marginBottom: '25px' }}>
+            <div style={{ height: '60px', marginTop: '1px', marginBottom: '5px' }}>
                 <div className={`alert alert-${props.obj.type}`} role="alert" style={{ border: '1px solid black' }}>
                     <strong>{props.obj.message}</strong>
                 </div>
